55e8aff558c30dca64d325d317e0dc2a54f9512a
antonyjes <72809149@continental.edu.pe>
2022-12-07 20:24:26 -0500

    Box CSS

---
+++ src/components/Box.jsx
@@ -0,0 +1,15 @@
+import '../styles/Box.css';
+
+function Box(){
+    return(
+        <div>
+            <div className="box">
+
+            </div>
+            
+            <input type="text" className='input-text'/>
+        </div>
+    )
+}
+
+export default Box;
